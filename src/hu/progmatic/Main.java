2111b840e0f1272b16459fcf513f09cc1595c4c0
TDDani <tothd0504@gmail.com>
2022-03-12 13:28:36 +0100

    G

--- src/hu/progmatic/Main.java
+++ src/hu/progmatic/Main.java
@@ -3,6 +3,11 @@ package hu.progmatic;
 public class Main {
 
     public static void main(String[] args) {
-	
+        System.out.println("Az én nevem Dani");
+
+        System.out.println("TTTTTTTTT");
+
+
+
     }
 }
