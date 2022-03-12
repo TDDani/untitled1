cf6ff236585eeef21f2bf093165b96319cd9a274
TDDani <tothd0504@gmail.com>
2022-03-12 14:17:48 +0100

    gitignore

--- src/hu/progmatic/Main.java
+++ src/hu/progmatic/Main.java
@@ -3,9 +3,7 @@ package hu.progmatic;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Az én nevem Dani");
 
-        System.out.println("TTTTTTTTT");
 
 
 
